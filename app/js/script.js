3a03f3b4e31aac11efdc8624a9e76954b30dff29
Daniel Hedima <shamzy647@gmail.com>
2025-05-01 10:54:01 +0100

    CSS Light mode

--- app/js/script.js
+++ app/js/script.js
@@ -8,8 +8,6 @@ const themeImg = document.getElementById("theme")
 
 
 
-
-
 themeImg.addEventListener('click', () => {
     if (themeImg.src.includes("icon-sun.svg")) {
         themeImg.src = "assets/images/icon-moon.svg"
