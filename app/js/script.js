ed7d1bf9533a70a290ffbc80ecb5033c3b1912a0
Daniel Hedima <shamzy647@gmail.com>
2025-05-15 12:21:37 +0100

    Added PseudoCode to explain the code.

--- app/js/script.js
+++ app/js/script.js
@@ -8,18 +8,35 @@ const removeButtons = document.querySelectorAll('.removebutton')
 
 
 /* || THEME TOGGLE */
+
+/* 
+1. This adds the lightmode theme class to the body, this overwrites the darkmode theme class that's the default.
+2. This stores the lightmode theme in local storage and makes the theme active.
+*/
 const enableLightMode = () => {
     document.body.classList.add('lightmode')
     localStorage.setItem('lightmode', 'active')
 }
 
+/* 
+1. This removes the lightmode theme class from the body and restores the darkmode theme.
+2. This removes the active state of the lightmode theme. */
 const disableLightMode = () => {
     document.body.classList.remove('lightmode')
     localStorage.setItem('lightmode', null)
 }
 
+/* 
+This line helps to save the current selected theme even when the website is refreshed. 
+1. If lightmode is active then it is enabled again.
+2. If lightmode is not active then the default dark theme is active.
+*/
 if (lightmode === "active") enableLightMode()
 
+/* 
+When the theme switch button is clicked, it gets the lightmode theme from local storage. 
+    If lightmode is not active then the enableLightMode() function is triggered. 
+    If it's active then the disableLightMode() function is triggered.*/
 themeSwitch.addEventListener('click', () => {
     // !== - Means "Does Not Equal", ? - Means "Then", : - Means "Else"
     lightmode = localStorage.getItem('lightmode')
@@ -27,6 +44,10 @@ themeSwitch.addEventListener('click', () => {
 })
 
 /* || SELECTED BUTTONS */
+
+/* 
+When the all button is clicked and it does not have the .selected class, it removes the .selected class from either the active button or inactive button, and it adds the .selected class to the all button.
+*/
 allBtn.addEventListener('click', () => {
     if (allBtn.className !== 'selected') {
         activeBtn.classList.remove('selected') || inactiveBtn.classList.remove('selected')
@@ -34,6 +55,9 @@ allBtn.addEventListener('click', () => {
     }
 })
 
+/* 
+When the active button is clicked and it does not have the .selected class, it removes the .selected class from either the all button or inactive button, and it adds the .selected class to the active button.
+*/
 activeBtn.addEventListener('click', () => {
     if (activeBtn.className !== 'selected') {
         allBtn.classList.remove('selected') || inactiveBtn.classList.remove('selected')
@@ -42,6 +66,9 @@ activeBtn.addEventListener('click', () => {
     }
 })
 
+/* 
+When the inactive button is clicked and it does not have the .selected class, it removes the .selected class from either the all button or active button, and it adds the .selected class to the inactive button.
+*/
 inactiveBtn.addEventListener('click', () => {
     if (inactiveBtn.className !== 'selected') {
         allBtn.classList.remove('selected') || activeBtn.classList.remove('selected')
@@ -51,6 +78,10 @@ inactiveBtn.addEventListener('click', () => {
 
 /* || ACTIVE AND INACTIVE EXTENSIONS */
 
+/* 
+1. If the checkbox is checked, the card should have the active class. If the inactive button is selected then add the hidden class to the card, else remove the hidden class if the active or all buttons are selected.
+2. If the checkbox is NOT checked, the card should NOT have the active class. If the active button is selected then add the hidden class to the card, else remove the hidden class if the inactive or all buttons are selected.
+*/
 const toggleCardStatus = (checkbox) => {
     const card = checkbox.closest('.card');
     const isActiveView = currentFilter === 'active';
@@ -67,8 +98,14 @@ const toggleCardStatus = (checkbox) => {
     }
 }
 
+/* This displays all the cards at default, whether active or inactive. */
 let currentFilter = 'all';
 
+/* 
+This creates a filterCards function whenever any of the buttons are clicked, and assigns a parameter(type) to the currentFilter variable.
+    1. If the currentFilter(type) is all, then remove the hidden class from all cards.
+    2. If the currentFilter(type) is active, it adds the hidden class to those cards that are NOT active, so only the active cards are displayed.
+    3. If the currentFilter(type) is inactive, it adds the hidden class to those cards that are active, so only the inactive cards are displayed.  */
 const filterCards = (type) => {
     currentFilter = type;
     const cards = document.querySelectorAll('.card');
@@ -87,9 +124,12 @@ const filterCards = (type) => {
 
 /* || REMOVE BUTTON */
 
+/* 
+For each button in the removeButtons list, remove the card when it's button is clicked */
 removeButtons.forEach(button => {
     button.addEventListener('click', () => {
         const card = button.parentElement
         card.parentElement.remove()
     })
 })
+
